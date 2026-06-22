b010ac72226ea87793796fddbaf3f782c278cfb2
Vitorgg <vitoribadaro@gmail.com>
2026-06-22 01:38:54 +0000

    Adicionar sistema de cadastro V2 com menu interativo

---
+++ bash/cadastro_v2.sh
@@ -0,0 +1,54 @@
+echo "==========================="
+echo "   SISTEMA DE CADASTRO V2  "
+echo "==========================="
+echo
+
+nome=""
+idade=""
+profissao=""
+
+while true
+do
+
+echo " 1 - Cadastra usuário"
+echo " 2 - Mostrar cadastro"
+echo " 3 - Apagar cadastro"
+echo " 4 - Sair"
+echo
+echo -n "Escolha: "
+read  escolha
+echo
+
+case $escolha in
+
+1)
+	echo -n "Nome: "$nome
+	read nome
+	echo -n "Idade: "$idade
+	read idade
+	echo -n "Profissão: "$profissao
+	read profissao
+	;;
+2)
+	if [  -z "$nome" ]
+	then
+		echo "Nenhum cadastro encontrado!"
+	else
+		echo "Usuário: "$nome
+		echo "Idade: "$idade
+		echo "Profissão: "$profissao
+	fi
+	;;
+3)
+
+	nome=""
+	idade=""
+	profissao=""
+	echo "Informações apagadas!"
+	;;
+4)
+	break
+	echo "Encerrando Sistema...."
+	;;
+esac
+done
